46103959e979c2a64e1fea2a1ea100f2a78ade0b
pnzrkmpfwgn <kurtknispel9@gmail.com>
2021-07-01 10:51:44 +0300

    created some dynamic relation with urls

--- components/urls.js
+++ components/urls.js
@@ -1,23 +1,23 @@
 export const base_url = "http://localhost:1337";
-export const images_url = "http://localhost:1337/images";
-export const nav_url = "http://localhost:1337/main-menu";
-export const en_nav_url = "http://localhost:1337/en-main-menu"
-export const en_summary_url = "http://localhost:1337/en-summaries";
-export const tr_summary_url = "http://localhost:1337/tr-summaries";
-export const en_adresses_url = "http://localhost:1337/en-adresses";
-export const tr_adresses_url = "http://localhost:1337/tr-adresses";
-export const social_media_url = "http://localhost:1337/social-medias";
-export const tr_about_url = "http://localhost:1337/tr-abouts";
-export const en_about_url = "http://localhost:1337/en-abouts";
-export const offices_url="http://localhost:1337/offices";
-export const en_offices_url="http://localhost:1337/en-offices";
-export const contact_url="http://localhost:1337/contacts";
-export const en_contact_url="http://localhost:1337/en-contacts";
-export const tr_rss_url = "http://localhost:1337/tr-getrssinfo";
-export const en_rss_url = "http://localhost:1337/en-getrssinfo";
-export const tr_slides_url = "http://localhost:1337/tr-sliders";
-export const en_slides_url = "http://localhost:1337/en-sliders";
-export const tr_posts_url = "http://localhost:1337/posts";
-export const en_posts_url = "http://localhost:1337/en-posts";
-export const tr_post_url_limited = "http://localhost:1337/posts?limit=4"
-export const en_post_url_limited = "http://localhost:1337/en-posts?limit=4"
+export const images_url = `${base_url}/images`;
+export const nav_url = `${base_url}/main-menu`;
+export const en_nav_url = `${base_url}/en-main-menu`;
+export const en_summary_url = `${base_url}/en-summaries`;
+export const tr_summary_url = `${base_url}/tr-summaries`;
+export const en_adresses_url = `${base_url}/en-adresses`;
+export const tr_adresses_url = `${base_url}/tr-adresses`;
+export const social_media_url = `${base_url}/social-medias`;
+export const tr_about_url = `${base_url}/tr-abouts`;
+export const en_about_url = `${base_url}/en-abouts`;
+export const offices_url=`${base_url}/offices`;
+export const en_offices_url=`${base_url}/en-offices`;
+export const contact_url=`${base_url}/contacts`;
+export const en_contact_url=`${base_url}/en-contacts`;
+export const tr_rss_url = `${base_url}/tr-getrssinfo`;
+export const en_rss_url = `${base_url}/en-getrssinfo`;
+export const tr_slides_url = `${base_url}/tr-sliders`;
+export const en_slides_url = `${base_url}/en-sliders`;
+export const tr_posts_url = `${base_url}/posts`;
+export const en_posts_url = `${base_url}/en-posts`;
+export const tr_post_url_limited = `${base_url}/posts?limit=4`
+export const en_post_url_limited = `${base_url}/en-posts?limit=4`
